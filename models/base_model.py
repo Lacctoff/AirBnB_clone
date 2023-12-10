34c1d4eb41dad32def165e4621ae1105d1c39eb7
Lacctoff <lasisitofunmi@gmail.com>
2023-12-10 13:36:34 -0800

    Testing base_models

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module containing the BaseModel class
+"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        Assigns a unique id and sets created_at and updated_at attributes.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
